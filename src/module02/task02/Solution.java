af2f3d2bf096a81fc9403fd08720fbdd8174ca1c
Malikov <malikov.yuriy.alex@gmail.com>
2016-08-22 21:51:12 +0300

    Module 2 task 1 commit

--- src/module02/task02/Solution.java
+++ src/module02/task02/Solution.java
@@ -2,4 +2,77 @@ package module02.task02;
 
 
 public class Solution {
+
+    //HOMEWORK 2.1
+
+    // It can remove unnecessary zeros in the end of double
+    public static String getGoodLookingDoubleForPrint(double d) {
+        int i = (int) d;
+        return i == d ? String.valueOf(i) : String.valueOf(d);
+    }
+
+    static double withdrawBalance(double balance, double withdrawal) {
+        double newBalance = balance - withdrawal * 1.05;
+
+        if (newBalance < 0)
+            System.out.println("NO");
+        else
+            System.out.println("YES " + getGoodLookingDoubleForPrint(withdrawal * 0.05) + " " +
+                    getGoodLookingDoubleForPrint(newBalance));
+
+        return newBalance;
+    }
+
+    //HOMEWORK 2.2
+
+    static String[] getOwnerNamesArray() {
+        return new String[]{"Jane", "Ann", "Jack", "Oww", "Lane"};
+    }
+
+    static int[] getBalancesArray() {
+        return new int[]{1200, 250, 2000, 500, 3200};
+    }
+
+    // method returns -1 if fails
+    static double withdrawBalance(String ownerName, double withdrawal) {
+        String[] names = getOwnerNamesArray();
+        int[] balances = getBalancesArray();
+        for (int i = 0; i < names.length; i++)
+            if (ownerName.equals(names[i])) {
+                double newBalance = balances[i] - withdrawal * 1.05;
+                if (newBalance < 0)
+                    System.out.println("NO " + names[i]);
+                else
+                    System.out.println(names[i] + " " + getGoodLookingDoubleForPrint(withdrawal) + " " +
+                            getGoodLookingDoubleForPrint(newBalance));
+            }
+        return -1;
+    }
+
+    //HOMEWORK 2.3
+
+    // method returns -1 if fails
+    static double fundBalance(String ownerName, double fund) {
+        String[] names = getOwnerNamesArray();
+        int[] balances = getBalancesArray();
+        for (int i = 0; i < names.length; i++)
+            if (ownerName.equals(names[i]))
+                System.out.println(names[i] + " " + getGoodLookingDoubleForPrint(balances[i] + fund));
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        int[] balances1 = {200, 200, 500, 4000, 30};
+
+        // some testing
+        withdrawBalance(100, 10);
+        withdrawBalance(100, 99);
+        withdrawBalance("Ann", 100);
+        withdrawBalance("Oww", 490);
+        fundBalance("Oww", 100);
+    }
 }
