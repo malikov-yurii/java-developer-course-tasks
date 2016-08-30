92be3b8ee2611948910e3fe49f07934f001448ec
Malikov <malikov.yuriy.alex@gmail.com>
2016-08-30 09:53:51 +0300

    25 Aug 2016 module 3 task 1.   version 2.0, all  tests 100%

--- src/module03/task01/Bird.java
+++ src/module03/task01/Bird.java
@@ -1,11 +1,14 @@
 package module03.task01;
 
-public class Bird {
-    void sing() {
-        System.out.println("I am singing");
+public class Bird extends Animal {
+    void sing() {System.out.println("I am singing");}
+
+    void fly() {System.out.println("I am flying");}
+
+    Bird(){
+        this.walk();
+        this.fly();
+        this.sing();
         System.out.println("I am Bird");
-    }
-    void fly() {
-        System.out.println("I am flying");
     }
 }
--- src/module03/task01/Solution.java
+++ src/module03/task01/Solution.java
@@ -2,9 +2,6 @@ package module03.task01;
 
 public class Solution {
     public static void main(String[] args) {
-        new Animal().walk();
         Bird bird = new Bird();
-        bird.fly();
-        bird.sing();
     }
 }
