f5211798ef84be40db2b307688864aeea4ef91f3
Malikov <malikov.yuriy.alex@gmail.com>
2016-08-30 14:11:50 +0300

    25 Aug 2016 module 3 task 1.   version 2.0, all  tests 100% module 3 task 2.   version 2.0, server error module 3 task 3.   version 1.0

--- src/module03/task03/CollegeStudent.java
+++ src/module03/task03/CollegeStudent.java
@@ -1,12 +1,51 @@
 package module03.task03;
 
 
-public class CollegeStudent {
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+public class CollegeStudent extends Student {
+    private String collegeName;
+    private int rating;
+    private long id;
+
+    public CollegeStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public CollegeStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public CollegeStudent(String firstName, String lastName, int group, Course[] coursesTaken,
+                            int age, String collegeName, int rating, long id){
+        super(firstName, lastName, group);
+        this.setCoursesTaken(coursesTaken);
+        this.setAge(age);
+        this.setCollegeName(collegeName);
+        this.setRating(rating);
+        this.setId(id);
+
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- src/module03/task03/SpecialStudent.java
+++ src/module03/task03/SpecialStudent.java
@@ -1,11 +1,20 @@
 package module03.task03;
 
-public class SpecialStudent {
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+public class SpecialStudent extends CollegeStudent {
+    private long secretKey;
+    private String email;
+
+    public SpecialStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public SpecialStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public SpecialStudent(String firstName, String lastName, int group, Course[] coursesTaken,
+                          int age, String collegeName, int rating, long id, long secretKey) {
+        super(firstName, lastName, group, coursesTaken, age, collegeName, rating, id);
+        this.secretKey = secretKey;
+    }
 }
