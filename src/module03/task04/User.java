f5a2ef4f769b0aa7873a18527ba2b34e9de0bd63
Malikov <malikov.yuriy.alex@gmail.com>
2016-08-30 14:25:21 +0300

    25 Aug 2016 module 3 task 1.   version 2.0, all  tests 100% module 3 task 2.   version 2.0, server error module 3 task 3.   version 1.0 module 3 task 4.   version 1.0

--- src/module03/task04/User.java
+++ src/module03/task04/User.java
@@ -1,16 +1,72 @@
 package module03.task04;
 
 public class User {
-    //String name;
-    //int balance;
-    //Date startOfWork;
-    //
-    //
-    //
-    //
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
 
-    //a few constructors
+    public void paySalary(){this.setBalance(this.getBalance() + this.getSalary());}
+    public void withdraw(int summ){this.setBalance(this.getBalance() - (int)(summ < 1000 ? summ * 1.05 : summ * 1.1));}
+    public void companyNameLenfht(){this.setCompanyName("Goodwin Ink.");}
+    public void monthIncreaser(int addMonth){this.setMonthsOfEmployment(this.getMonthsOfEmployment() + addMonth);}
 
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
