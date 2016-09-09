9cfd1edbf7801860b7e3a1110705475967cad36a
Malikov <malikov.yuriy.alex@gmail.com>
2016-09-09 16:47:43 +0300

    task 4.5  v1.0

--- src/module04/BankSystem.java
+++ src/module04/BankSystem.java
@@ -1,4 +1,8 @@
 package module04;
 
 public interface BankSystem {
+    void withdrawOfUser(User user, int amount);
+    void fundUser(User user, int amount);
+    void transferMoney(User fromUser, User toUser, int amount);
+    void paySalary(User user);
 }
--- src/module04/BankSystemImpl.java
+++ src/module04/BankSystemImpl.java
@@ -1,4 +1,23 @@
 package module04;
 
 public class BankSystemImpl implements BankSystem {
+    @Override
+    public void withdrawOfUser(User user, int amount) {
+        
+    }
+
+    @Override
+    public void fundUser(User user, int amount) {
+
+    }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+
+    }
+
+    @Override
+    public void paySalary(User user) {
+
+    }
 }
