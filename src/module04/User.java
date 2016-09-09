8f041bd2d23ed38bd742cac43cfb400397f21d0d
Malikov <malikov.yuriy.alex@gmail.com>
2016-09-09 16:43:23 +0300

    task 4.4 v1.0

--- src/module04/User.java
+++ src/module04/User.java
@@ -1,4 +1,24 @@
 package module04;
 
 public class User {
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", monthsOfEmployment=" + monthsOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
 }
