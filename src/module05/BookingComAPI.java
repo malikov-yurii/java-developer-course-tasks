0d82c362aa52f364042cc9c62cc56ceb9f3963fe
Malikov <malikov.yuriy.alex@gmail.com>
2016-09-17 14:01:06 +0300

    task 5.4  v0.1

---
+++ src/module05/BookingComAPI.java
@@ -0,0 +1,22 @@
+package module05;
+
+import java.util.Date;
+
+public class BookingComAPI implements RoomAPI {
+    private Room[] rooms;
+
+    public BookingComAPI() {
+        rooms = new Room[]{
+            new Room(1, 200, 2, new Date(2000, 11 , 20), "Metropol", "NY"),
+            new Room(2, 200, 2, new Date(2000, 11 , 21), "Metropol", "NY"),
+            new Room(3, 300, 3, new Date(2000, 11 , 22), "Metropol", "NY"),
+            new Room(4, 300, 3, new Date(2000, 11 , 23), "Metropol", "NY"),
+            new Room(5, 400, 4, new Date(2000, 11 , 24), "Metropol", "NY"),
+        };
+    }
+
+    @Override
+    public Room[] findRooms(int price, int person, String city, String hotel) {
+        return null;
+    }
+}
--- src/module05/RoomAPI.java
+++ src/module05/RoomAPI.java
@@ -1,5 +1,5 @@
 package module05;
 
-public interface roomAPI {
+public interface RoomAPI {
     Room[] findRooms(int price, int person, String city, String hotel);
 }
