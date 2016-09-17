aa85b916e7c05fd67374055e5da0541cc670deee
Malikov <malikov.yuriy.alex@gmail.com>
2016-09-17 13:42:55 +0300

    task 5.2  v0.1

--- src/module05/Room.java
+++ src/module05/Room.java
@@ -4,17 +4,57 @@ import java.util.Date;
 
 public class Room {
     private long id;
+    private int price;
     private int persons;
     private Date dateAvailableFrom;
     private String hotelName;
     private String cityName;
 
+    public Room(long id, int price, int persons, Date dateAvailableFrom, String hotelName, String cityName) {
+
+        this.id = id;
+        this.price = price;
+        this.persons = persons;
+        this.dateAvailableFrom = dateAvailableFrom;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        if (price != room.price) return false;
+        if (persons != room.persons) return false;
+        return cityName.equals(room.cityName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = price;
+        result = 31 * result + persons;
+        result = 31 * result + cityName.hashCode();
+        return result;
+    }
+
     public long getId() {
+
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     public int getPersons() {
@@ -46,15 +86,6 @@ public class Room {
     }
 
     public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-
-    public Room(long id, int persons, Date dateAvailableFrom, String hotelName, String cityName) {
-
-        this.id = id;
-        this.persons = persons;
-        this.dateAvailableFrom = dateAvailableFrom;
-        this.hotelName = hotelName;
         this.cityName = cityName;
     }
 }
