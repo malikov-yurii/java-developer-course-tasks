fcbc161435a41dbe6be3b1fea5ae0202a17bb725
Malikov <malikov.yuriy.alex@gmail.com>
2016-09-24 08:30:30 +0300

    task 6.4  v0.1

---
+++ src/module06/User.java
@@ -0,0 +1,17 @@
+package module06;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+}
