e9ea15cb3353a37ec4ba5dd64c0c3d7025f0ff22
Malikov <malikov.yuriy.alex@gmail.com>
2016-10-20 18:22:34 +0300

    task 9.1 v0.1

--- src/module07/task02/Main.java
+++ src/module07/task02/Main.java
@@ -17,10 +17,13 @@ public class Main {
                 new Order(14, 13300, module07.Currency.UAH, "Lamp4", "Godman Store4", new User(1114, "Bob4", "Marley4", "Kiev", 1004)),
                 new Order(15, 12200, module07.Currency.UAH, "Lamp5", "Godman Store222", new User(1115, "Bob5", "Marley5", "Rovno", 1005)),
                 new Order(16, 12200, module07.Currency.USD, "Lamp6", "Godman Store6", new User(1116, "Bob6", "Marley6", "Kiev", 1006)),
+                new Order(137, 200, module07.Currency.UAH, "Lamp7", "Godman Store7", new User(1117, "Bob7", "Marley7", "Kamyzyak", 1007)),
+                new Order(173, 20, module07.Currency.UAH, "Lamp7", "Godman Store7", new User(1117, "Bob7", "Marley7", "Kamyzyak", 1007)),
                 new Order(17, 22200, module07.Currency.UAH, "Lamp7", "Godman Store7", new User(1117, "Bob7", "Marley7", "Kamyzyak", 1007)),
-                new Order(18, 11, module07.Currency.USD, "Lamp8", "Godman Store8", new User(1118, "Bob8", "Marley8", "Rovno", 1008))
+                new Order(18, 11, module07.Currency.USD, "Lamp8", "Godman Store8", new User(1118, "Bob8", "Marley8", "Rovno", 1008)),
+                new Order(18, 11, module07.Currency.USD, "Lamp8", "Godman Store8", new User(1118, "Bob8", "Petrov", "Rovno", 1008))
         );
-
+/*
         orders.sort((o1, o2) -> o2.getPrice() - o1.getPrice());
         System.out.println("\nOrders are sorted with price (with descending order): \n" + orders);
 
@@ -66,9 +69,7 @@ public class Main {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-        Map<Currency, List<Order>> ordersByCurrency = orders.stream().collect(Collectors.groupingBy(order -> order.getCurrency()));
-
-/*
+*//*
         List<Order> uahCurrency = new ArrayList<>();
         List<Order> usdCurrency = new ArrayList<>();
         for (Order order : orders)
@@ -76,7 +77,7 @@ public class Main {
                 uahCurrency.add(order);
             else
                 usdCurrency.add(order);
-*/
+*//*
         System.out.println("\nOrders by currency: \n");
         for (Map.Entry<Currency, List<Order>> entry : ordersByCurrency.entrySet())
             System.out.println(entry.getValue());
@@ -84,17 +85,61 @@ public class Main {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         Map<String, List<Order>> ordersByCity = orders.stream().collect(Collectors.groupingBy(order -> order.getUser().getCity()));
-/*
+*//*
         Map<String, List<Order>> ordersByCity = new HashMap<>();
         for (Order order : orders) {
             if (!ordersByCity.containsKey(order.getUser().getCity()))
                 ordersByCity.put(order.getUser().getCity(), new ArrayList<>());
             ordersByCity.get(order.getUser().getCity()).add(order);
         }
-*/
+*//*
         System.out.println("\nOrders by city: \n");
         for (Map.Entry<String, List<Order>> entry : ordersByCity.entrySet()) {
             System.out.println(entry.getValue());
         }
+        */
+
+        /////////////////////////////////    9.1     ////////////////////////////////////////////////////////////////
+
+        Comparator<Order> byPrice = (o1, o2) -> o2.getPrice() - o1.getPrice();
+        System.out.println("\n       9.1\nOrders are sorted with price (with descending order): \n"
+                + orders.stream().sorted(byPrice.reversed()).collect(Collectors.toList()));
+
+        Comparator<Order> byCity = (o1, o2) -> o1.getUser().getCity().compareTo(o2.getUser().getCity());
+        System.out.println("\nOrders are sorted with price and city: \n" +
+                orders.stream().sorted(byPrice.thenComparing(byCity)).collect(Collectors.toList()));
+
+        Comparator<Order> byItemName = (o1, o2) -> o1.getItemName().compareTo(o2.getItemName());
+        Comparator<Order> byShopIdentificator = (o1, o2) -> o1.getShopIdentificator().compareTo(o2.getShopIdentificator());
+        System.out.println("\nOrders are sorted with itemName and shopName and city: \n" +
+                orders.stream().sorted(byItemName.thenComparing(byShopIdentificator.thenComparing(byCity))).collect(Collectors.toList()));
+
+        System.out.println("\nDuplicates are deleted: \n" +
+                orders.stream().distinct().collect(Collectors.toList()));
+
+        System.out.println("\nPrice less than 1500: \n");
+        orders.stream().filter(order -> order.getPrice() < 1500).forEach(System.out::print);
+
+        System.out.println("\nOrders by currency:");
+        orders.stream()
+                .collect(Collectors.groupingBy(order -> order.getCurrency()))
+                .forEach((currency, ordersByCurrency) -> {
+                    System.out.println("\n" + currency + " list:\n");
+                    ordersByCurrency.forEach(System.out::print);
+                });
+
+        System.out.println("\nOrders by city:");
+        orders.stream()
+                .collect(Collectors.groupingBy(order -> order.getUser().getCity()))
+                .forEach((city, ordersByCurrency) -> {
+                    System.out.println("\n" + city + " list:\n");
+                    ordersByCurrency.forEach(System.out::print);
+                });
+
+        System.out.println("\n" + (orders.stream().anyMatch(order -> "Petrov".equals(order.getUser().getLastName())) ?
+        "Yes, we have order for Petrov." : "No, we have no order for Petrov."));
+
+        System.out.println("\nUSD orders are deleted:\n\n" +
+        orders.stream().filter(order -> !order.getCurrency().equals(Currency.USD)).collect(Collectors.toList()));
     }
 }
--- src/testpackage/A.java
+++ src/testpackage/A.java
@@ -3,13 +3,32 @@ package testpackage;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class A {
     public static void main(String[] args) {
-//        List<String> myList = Arrays.asList("a", "b");
-//        myList.add("c");
-//        System.out.println(myList.getClass());
+
+        class Person {
+            String firstName;
+            String lastName;
+
+            Person() {}
+
+            Person(String firstName, String lastName) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+        }
+
+        Comparator<Person> comparator = (p1, p2) -> p1.firstName.compareTo(p2.firstName);
+
+        Person p1 = new Person("John", "Doe");
+        Person p2 = new Person("Alice", "Wonderland");
+
+        System.out.println(comparator.compare(p1, p2));             // > 0
+        System.out.println(comparator.reversed().compare(p1, p2));
+        System.out.println("JoHn".compareTo("Alice"));
     }
 
 }
