cfb2094a83de29f961695acfe9a4470a319473c7
Malikov <malikov.yuriy.alex@gmail.com>
2016-10-25 22:54:57 +0300

    task 10.5 v1.0

--- src/module10/Task05.java
+++ src/module10/Task05.java
@@ -5,7 +5,7 @@ import java.nio.file.FileAlreadyExistsException;
 import java.util.Random;
 
 public class Task05 {
-    public static void newMethod(int i) throws Exception{
+    public static void newMethod(int i) throws FileAlreadyExistsException, FileNotFoundException, NullPointerException{
         switch (i) {
             case 1: throw new FileAlreadyExistsException("My new type that extends FileAlreadyExistException") {};
             case 2: throw new FileNotFoundException("My new type that extends FileNotFoundException") {};
@@ -15,6 +15,10 @@ public class Task05 {
     public static void main(String[] args){
         try{
             newMethod(new Random().nextInt(3) + 1);
-        } catch (Throwable e){e.printStackTrace();}
+        }
+        catch (FileAlreadyExistsException e){System.out.println("File already exists exeption: " + e.getMessage());}
+        catch (FileNotFoundException e){System.out.println("File not found exeption: " + e.getMessage());}
+        catch (NullPointerException e){System.out.println("Null pointer exeption: " + e.getMessage());}
+
     }
 }
