7672206f5252052e48850efd3f67cab020ea574f
Malikov <malikov.yuriy.alex@gmail.com>
2016-10-25 19:11:59 +0300

    task 10.4 v0.1

---
+++ src/module10/Task04.java
@@ -0,0 +1,20 @@
+package module10;
+
+public class Task04 extends Exception {
+    public static void main(String[] args) throws Task04{
+        f();
+    }
+
+    public static void f() throws Task04 {
+        try {
+            g();
+        } catch (Task04 e) {
+            throw new RuntimeException("My Runtime Exception"){};
+        }
+    }
+
+    public static void g() throws Task04 {
+        throw new Task04();
+    }
+
+}
