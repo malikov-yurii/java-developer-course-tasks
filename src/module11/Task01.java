66f86a95a3c3ab680184d01f334b8efc9d6eb07b
Malikov <malikov.yuriy.alex@gmail.com>
2016-11-01 18:17:51 +0200

    module 11 task 1  v0.1

---
+++ src/module11/Task01.java
@@ -0,0 +1,36 @@
+package module11;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task01 {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> map = new HashMap<>();
+        map.put("to", "TOTOTO");
+        map.put("a", "AAA");
+
+        System.out.println("Formatted file:\n" + new Task01().replacer(map));
+    }
+
+    public String replacer(Map<String, String> map) throws IOException {
+/*
+        System.out.print("Enter file name: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        byte[] encoded = Files.readAllBytes(Paths.get(br.readLine()));
+        br.close();
+*/
+        byte[] encoded = Files.readAllBytes(Paths.get("src/module11/1.txt"));
+
+        String fileText = new String(encoded, StandardCharsets.UTF_8);
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            fileText = fileText.replaceAll("\\b" + entry.getKey() + "\\b", entry.getValue());
+        }
+
+        return fileText;
+    }
+}
